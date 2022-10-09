fcd46b22313a69cc208bb1f3e6582e09113923c9
unknown <solanank@gmail.com>
2022-10-09 14:48:22 +0530

    newsletter is added

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 //calling modlues
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https"); //importing http module
 const _ = require('lodash');
 const ejs = require("ejs");
 //setting content for each page
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  "A web developer who likes to keep and simple and clean. Currently doing BCA from Maharaja Surajmal Institute ";
 //declaring onjects
 var blogPosts = [];
 
@@ -32,14 +31,13 @@ app.get("/", function (res, res) {
 app.get("/about", function (res, res) {
   res.render("about", { aboutContentEJS: aboutContent });
 });
-//get request for contact routr
-app.get("/contact", function (res, res) {
-  res.render("contact", { contactContentEJS: contactContent });
-});
+
+
 //get requet for compose route
 app.get("/compose", function (req, res) {
   res.render("compose", { contactContentEJS: contactContent });
 });
+
 //get for parameters
 app.get("/blogs/:postName",function(req ,res){
   
@@ -59,6 +57,63 @@ res.render("blog",{postTitleOut: postTitleIn  , postBodyOut: postBodyIn} )
     }
   })
  
+})
+//signup feature
+app.get("/newsletter", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+  //to show sign up [o
+});
+app.post("/newsletter", function (req, res) {
+  //taking data from form  with body-parser
+  const firstNames = req.body.firstName;
+  const SecondNames = req.body.secondName;
+  const emails = req.body.email;
+  
+  const data = {
+    members: [
+      {//declaring members as js object
+        email_address: emails,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstNames,
+          LNAME: SecondNames,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);//js object to json 
+  //declaring vars for posting ours members
+  const url = "https://us8.api.mailchimp.com/3.0/lists/2f5114433e";
+  const options = {
+    method: "POST",
+    auth: "ankur:a961a24d2eeb6173fb9d3025b7a3ff78-us8",
+  };
+  
+  const request = https.request(url, options, function (response) {
+    if(response.statusCode === 200){
+      
+        res.sendFile(__dirname + "/success.html")
+      //sending sucess on code 200
+    }
+    else{
+        res.sendFile(__dirname + "/failure.html")
+        //sending failure if something goes wrong
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  //sending the request dta to chipmunk
+  request.write(jsonData);
+  request.end();
+});
+//redirecting to the the / on failure
+app.post("/success",function(req,res){
+  res.redirect("/")
+})
+//redirecting to the the / on failure
+app.post("/failure",function(req,res){
+  res.redirect("/newsletter")
 })
 //post for compose
 app.post("/compose", function (req, res) {
